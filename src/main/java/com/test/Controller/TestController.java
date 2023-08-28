a11d1da573683b3bca0dcf159bd8e7dcef015436
jiawei.jiang <2066015243@qq.com>
2023-08-28 10:15:56 +0800

    test

---
+++ src/main/java/com/test/Controller/TestController.java
@@ -0,0 +1,19 @@
+package com.test.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: zhmen
+ * @CreateTime: 2023/8/28 $ {TIME}
+ * @Description:
+ */
+@RestController("test")
+public class TestController {
+
+    @GetMapping("hello")
+    public String Hello(){
+        return "<h1>Hello World</h1>";
+    }
+
+}
