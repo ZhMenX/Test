a457f578aa6cfe85b7a4c1bb94c642d9578d6752
jiawei.jiang <2066015243@qq.com>
2023-08-29 16:05:01 +0800

    upd

--- trigger_jenkins.ps1
+++ trigger_jenkins.ps1
@@ -3,8 +3,12 @@ $jenkinsUrl = "http://101.42.53.76:8080"
 $jobName = "Test-RemoteBuild"
 $token = "jiawei"
 
+# Get Jenkins crumb using Invoke-RestMethod
+$crumbUrl = "$jenkinsUrl/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)"
+$crumbHeader = Invoke-RestMethod -Uri $crumbUrl -Method Get -Headers @{Authorization=("Basic {0}" -f [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":11760f7590e9230bca04bc3d7a36a5e32d")))}
+
 # Construct the URL to trigger Jenkins build
 $buildUrl = "$jenkinsUrl/job/$jobName/build?token=$token"
 
-# Trigger the build using Invoke-RestMethod
-Invoke-RestMethod -Uri $buildUrl -Method Post
+# Trigger the build using Invoke-RestMethod with crumb header
+Invoke-RestMethod -Uri $buildUrl -Method Post -Headers $crumbHeader
